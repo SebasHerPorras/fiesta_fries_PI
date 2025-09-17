c21e0398b942a7c516375f0ffce6429d384be20c
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-09-16 22:38:14 -0600

    FrontEnd y BackEnd base

---
+++ Sprint-1/backend/Repositories/CountryRepository.cs
@@ -0,0 +1,48 @@
+using backend.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace backend.Handlers
+
+{
+    namespace backend.Repositories
+    {
+        public class CountryRepository
+        {
+            private readonly string _connectionString;
+            public CountryRepository()
+            {
+                var builder = WebApplication.CreateBuilder();
+                _connectionString = builder.Configuration.GetConnectionString("CountryContext");
+            }
+
+            public List<CountryModel> GetCountries()
+            {
+                using var connection = new SqlConnection(_connectionString);
+                string query = "SELECT * FROM dbo.Country";
+                return connection.Query<CountryModel>(query).ToList();
+            }
+
+            public bool CreateCountry(CountryModel country)
+            {
+                using var connection = new SqlConnection(_connectionString);
+
+                var query = @"INSERT INTO [dbo].[Country] 
+				  ([Name],[Language],[Continent])  
+				  VALUES(@Name, @Language, @Continent)";
+
+                var affectedRows = connection.Execute(query, new
+                {
+                    Name = country.Name,
+                    Language = country.Language,
+                    Continent = country.Continent
+                });
+
+                return affectedRows >= 1;
+            }
+
+        }
+    }
+}
+
