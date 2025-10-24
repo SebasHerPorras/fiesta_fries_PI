fd3cf58f69e0e3c403feb7e78e70c4af03fc1947
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-24 00:28:53 -0600

    Se agrega interfaz IPayrollProcessingService para gestionar el procesamiento de planilla (SCRUM-54)

---
+++ Sprint-1/backend/Services/IPayrollProcessingService.cs
@@ -0,0 +1,10 @@
+﻿using backend.Models.Payroll.Requests;
+using backend.Models.Payroll.Results;
+
+namespace backend.Interfaces.Services
+{
+    public interface IPayrollProcessingService
+    {
+        Task<PayrollProcessResult> ProcessPayrollAsync(PayrollProcessRequest request);
+    }
+}
