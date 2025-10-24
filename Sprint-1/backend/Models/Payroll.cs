4660c1af3d7b563edaef82004d488982452f1ede
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-23 22:43:19 -0600

    Se agrega modelo Payroll para gestionar planillas (SCRUM-54)

---
+++ Sprint-1/backend/Models/Payroll.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models.Payroll
+{
+    public class Payroll
+    {
+        public int PayrollId { get; set; }
+        public DateTime PeriodDate { get; set; }
+        public long CompanyId { get; set; }
+        public bool IsCalculated { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public string? ApprovedBy { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
