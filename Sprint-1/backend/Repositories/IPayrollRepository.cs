0688642b752a2926188509d79e7a22173515fd2b
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-23 23:56:02 -0600

    Se agrega interfaz IPayrollRepository para operaciones de planilla (SCRUM-54)

---
+++ Sprint-1/backend/Repositories/IPayrollRepository.cs
@@ -0,0 +1,8 @@
+﻿public interface IPayrollRepository
+{
+    Task<Payroll?> GetByPeriodAndCompanyAsync(DateTime periodDate, long companyId);
+    Task<int> CreatePayrollAsync(Payroll payroll);
+    Task UpdatePayrollAsync(Payroll payroll);
+    Task<List<PayrollPayment>> CreatePayrollPaymentsAsync(List<PayrollPayment> payments);
+    Task<List<PayrollPayment>> GetPayrollDetailsAsync(int payrollId);
+}
