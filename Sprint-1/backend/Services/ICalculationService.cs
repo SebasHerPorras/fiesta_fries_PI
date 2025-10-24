c78ba1225a0346051d123168786be3847435aacc
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-24 00:21:54 -0600

    Se agrega interfaz ICalculationService para definir servicios de cálculo de planilla (SCRUM-54)

---
+++ Sprint-1/backend/Services/ICalculationService.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Interfaces.Services
+{
+    public interface ICalculationService
+    {
+        Task<decimal> CalculateDeductionsAsync(int employeeId, long companyId, int payrollId);
+        Task<decimal> CalculateBenefitsAsync(int employeeId, long companyId, int payrollId);
+        Task<decimal> CalculateIncomeTaxAsync(int employeeId, long companyId, int payrollId);
+    }
+}
