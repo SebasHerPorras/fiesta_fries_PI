9f67114a12572c4a26977135811f48b3fb8cb70c
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-25 16:34:52 -0600

    Se agrega carpeta Infrastructure con componentes de conexión a base de datos (SCRUM-54)

---
+++ Sprint-1/backend/Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using backend.Interfaces;
+
+namespace backend.Infrastructure
+{
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("UserContext")
+                ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
