0f7792e2a0bd43ad9bef7547ec50589b3c6d5999
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-24 23:13:14 -0600

    Se agrega interfaz IPayrollResultBuilder (SCRUM-54)

---
+++ Sprint-1/backend/Services/IPayrollResultBuilder.cs
@@ -0,0 +1,10 @@
+﻿using backend.Models.Payroll.Results;
+
+namespace backend.Interfaces.Services
+{
+    public interface IPayrollResultBuilder
+    {
+        PayrollProcessResult CreateSuccessResult(int payrollId, PayrollProcessingResult processingResult);
+        PayrollProcessResult CreateErrorResult(string message);
+    }
+}
