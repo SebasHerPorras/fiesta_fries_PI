5a91752d3668bd6b329714181f383521f6d0f004
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-10-28 12:17:53 -0600

    Se realizan ajustes en IPayrollProcessingService relacionados con el proceso de planilla (SCRUM-54).

--- Sprint-1/backend/Interfaces/IPayrollProcessingService.cs
+++ Sprint-1/backend/Interfaces/IPayrollProcessingService.cs
@@ -1,10 +1,13 @@
 ﻿using backend.Models.Payroll.Requests;
 using backend.Models.Payroll.Results;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace backend.Interfaces.Services
 {
     public interface IPayrollProcessingService
     {
         Task<PayrollProcessResult> ProcessPayrollAsync(PayrollProcessRequest request);
+        Task<List<PayrollSummaryResult>> GetPayrollsByCompanyAsync(string companyId);
     }
 }
