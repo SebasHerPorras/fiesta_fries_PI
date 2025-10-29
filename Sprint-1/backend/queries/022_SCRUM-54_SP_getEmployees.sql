09f5f844c02ce1b6d1118220d16de1f37d8fa982
SebasHerPorras <hernandezporrassebastian@gmail.com>
2025-10-29 13:07:53 -0600

    Update 022_SCRUM-54_SP_getEmployees.sql

--- Sprint-1/backend/queries/022_SCRUM-54_SP_getEmployees.sql
+++ Sprint-1/backend/queries/022_SCRUM-54_SP_getEmployees.sql
@@ -14,23 +14,25 @@ BEGIN
     SELECT 
         p.id AS CedulaEmpleado,
         p.firstName + ' ' + p.secondName AS NombreEmpleado,
-        -- Calcular salario según tipo de empleado
+        -- Calcular salario según tipo de empleado y frecuencia de pago
         CASE 
             -- Por horas: salario base (tarifa por hora) * horas trabajadas
-            WHEN LOWER(LTRIM(RTRIM(e.employmentType))) = 'por horas' THEN 
+            WHEN LOWER(LTRIM(RTRIM(e.employmentType))) = 'Por Horas' THEN 
                 e.salary * dbo.Fn_ObtenerHoras(e.id, @FechaInicio, @FechaFin)
                 
-            -- Medio tiempo: salario base / 2
-            WHEN LOWER(LTRIM(RTRIM(e.employmentType))) = 'medio tiempo' THEN 
-                e.salary / 2.0
-                
-            -- Tiempo completo: salario base completo
-            WHEN LOWER(LTRIM(RTRIM(e.employmentType))) = 'tiempo completo' THEN 
-                e.salary
-                
-            -- Por defecto: salario base
+            -- Empleados regulares: ajustar según frecuencia de pago de la empresa
             ELSE 
-                e.salary
+                CASE 
+                    -- Pago quincenal: salario / 2
+                    WHEN LOWER(LTRIM(RTRIM(emp.FrecuenciaPago))) = 'Quincenal' THEN 
+                        e.salary / 2.0
+                    -- Pago mensual: salario completo
+                    WHEN LOWER(LTRIM(RTRIM(emp.FrecuenciaPago))) = 'Mensual' THEN 
+                        e.salary
+                    -- Por defecto: salario completo
+                    ELSE 
+                        e.salary
+                END
         END AS SalarioBruto,
         e.employmentType AS TipoEmpleado,
         p.birthdate AS Cumpleanos,
@@ -38,6 +40,7 @@ BEGIN
     FROM Empleado e
     INNER JOIN Persona p ON e.id = p.id
     INNER JOIN [User] u ON p.uniqueUser = u.PK_User
+    INNER JOIN Empresa emp ON e.idCompny = emp.CedulaJuridica
     WHERE e.idCompny = @CedulaJuridica
     ORDER BY e.department, p.firstName;
 END;
