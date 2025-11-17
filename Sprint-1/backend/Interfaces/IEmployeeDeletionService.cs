5366a1f8bc2a89aea7186c124090342e005a04c1
SebasHerPorras <hernandezporrassebastian@gmail.com>
2025-11-16 18:22:37 -0600

    SCRUM 99 interfaz de eliminar empoleados

---
+++ Sprint-1/backend/Interfaces/IEmployeeDeletionService.cs
@@ -0,0 +1,11 @@
+using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IEmployeeDeletionService
+    {
+        Task<EmployeeDeletionResult> DeleteEmpleadoAsync(int personaId, long companyId);
+
+        Task<EmployeePayrollStatus> CheckPayrollStatusAsync(int personaId);
+    }
+}
